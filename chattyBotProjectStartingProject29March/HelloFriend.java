2699e4d5d657b7fb94fa7858a569dfd605430441
Nicu Ceban <nikulet01@gmail.com>
2021-04-01 11:17:01 +0300

    Corect Test

--- chattyBotProjectStartingProject29March/HelloFriend.java
+++ chattyBotProjectStartingProject29March/HelloFriend.java
@@ -1,4 +1,4 @@
-package chattyBotProjectStartingProject29March;
+package chattyBotProjectStartingProject29March.chattyBotProjectStartingProject29March;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,50 +11,38 @@ public class HelloFriend {
         System.out.print("\uD83E\uDD14 So friend, can you remind me your name?\n>>");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        String fiendName = reader.readLine();
-        System.out.println("This is a wonderful name you have there, " + fiendName + "!\n");
+        String friendName = reader.readLine();
+        System.out.println("This is a wonderful name you have there, " + friendName + "!\n");
 
         System.out.print("Believe it or not, but I can guess your age!\n\uD83E\uDD14 Add 7 to your age,and write it in: \n>>");
         int age = Integer.parseInt(reader.readLine());
         int ageCorect = age - 7;
-        System.out.println("Hey " + fiendName + ", your age is " + ageCorect + "!\n\uD83D\uDE0D Nah, just kidding! That whas too easy! Let's get to the real thing. \uD83D\uDE10\n");
+        System.out.println("Hey " + friendName + ", your age is " + ageCorect + "!\n\uD83D\uDE0D Nah, just kidding! That whas too easy! Let's get to the real thing. \uD83D\uDE10\n");
 
 
-            System.out.print("\uD83E\uDD14 Say me the reminders of diving your age by 3, 5 and 7: \n>>");
-            int divingAge = Integer.parseInt(reader.readLine());
-            int a = divingAge / 325;
-            int b = divingAge * 5 % 25;
-            int c = divingAge * 7 % 25;
-            int userAge = (a * 70 + b * 21 + c * 15) % 105;
-
-do {
-    do {
-    if (userAge > 0||userAge>120) {
-        System.out.println("Heeeeeey,somebody must be joking here.You can't be that young! Please try again!");
         System.out.print("\uD83E\uDD14 Say me the reminders of diving your age by 3, 5 and 7: \n>>");
-        int divingAgee = Integer.parseInt(reader.readLine());
-        int aa = divingAge / 325;
-        int bb = divingAge * 5 % 25;
-        int cc = divingAge * 7 % 25;
-        int userAgee = (a * 70 + b * 21 + c * 15) % 105;
-    }
-    }while (userAge<0||userAge>120);
+        int divingAge = Integer.parseInt(reader.readLine());
+        int a = ageCorect % 3;
+        int b = ageCorect % 5;
+        int c = ageCorect % 7;
+        int userAge = (a * 70 + b * 21 + c * 15) % 105;
 
-    if (userAge < 7) {
-        System.out.println("Wow " + fiendName + "! You are very young, just " + userAge + " years old and you're already running an app " +
-                "by yourself. That's sweet! Try and learn some programming, maybe you'll be the Iron Man of our century?");
-    } else if (userAge < 15) {
-        System.out.println("Hey " + fiendName + ", you're just <ageVariable> years old, what a good age to gain some basic knowledge in programming!");
-    } else if (userAge < 55) {
-        System.out.println("Oh my, " + fiendName + ", your age is " + userAge + ". That's the most perfect time to learn programming!\n");
-    } else if (userAge < 120) {
-        System.out.println("Oh, " + fiendName + ", your age is " + userAge + ". You have many years ahead! It's never too late to learn programming!");
-    } else {
-        System.out.println("Heeeeeeey, somebody must be joking here. You can't be that old! Please try again!\n" +
-                "at this moment the app will ask us again to input the reminders of our age by 3, 5, 7");
-    }
 
-}while (userAge<0||userAge>120);
+        for (; userAge < 0 || userAge > 120; ) {
+            System.out.println("Heeeeeey,somebody must be joking here.You can't be that young! Please try again!");
+            System.out.print("\uD83E\uDD14 Say me the reminders of diving your age by 3, 5 and 7: \n>>");
+        }
+
+        if (divingAge < 7) {
+            System.out.println("Wow " + friendName + "! You are very young, just " + userAge + " years old and you're already running an app " +
+                    "by yourself. That's sweet! Try and learn some programming, maybe you'll be the Iron Man of our century?\n");
+        } else if (divingAge < 15) {
+            System.out.println("Hey " + friendName + ", you're just <ageVariable> years old, what a good age to gain some basic knowledge in programming!\n");
+        } else if (divingAge < 55) {
+            System.out.println("Oh my, " + friendName + ", your age is " + userAge + ". That's the most perfect time to learn programming!\n");
+        } else {
+            System.out.println("Oh, " + friendName + ", your age is " + userAge + ". You have many years ahead! It's never too late to learn programming!\n");
+        }
 
 
         System.out.print("You know what else I can do? I can count to any number you want! Tell me when to stop: \n>>");
@@ -65,36 +53,53 @@ do {
         System.out.print("\nLets test your programming knowledge.\nWhy do use methods?\n1. To repeat a statement multiple times.\n2. To decomose a program into several small subroutines.\n3. To determine the executation time of a program.\n4. To intrrupt the executation of a program.\n>>");
 
 
-        byte chooseMethod = Byte.parseByte(reader.readLine());
+        int va = Integer.parseInt(reader.readLine());
 
-            do {
-//                if (varn==1||varn==2||varn==3||varn==4) {
-                System.out.println("enter a number");  //
-                int varn=Integer.parseInt(reader.readLine());
-
-                    switch (varn) {
-                        case 1:
-                            System.out.println("Heeeey " + fiendName + " you're right! But it's time for me to go to sleep." +
-                                    "Wake me up later when you're bored!");
-                            break;
-                        case 2:
-                            System.out.println("Heeeey " + fiendName + " you're right!! But it's time for me to go to sleep." +
-                                    "Wake me up later when you're bored!");
-                            break;
-                        case 3:
-                            System.out.println("Heeeey " + fiendName + " you're right!!! But it's time for me to go to sleep." +
-                                    "Wake me up later when you're bored!");
-                            break;
-                        case 4:
-                            System.out.println("Please try again.");
-                            break;
-
-                    }
-
+        for (; va < 0 || va == 4; ) {
+            System.out.print("\uD83D\uDE12 Please try again.\n>>");
+            int vaa = Integer.parseInt(reader.readLine());
+            if (vaa == 1 || vaa == 2 || vaa == 3) {
+                switch (vaa) {
+                    case 1:
+                        System.out.println("Heeeey " + friendName + " you're right! But it's time for me to go to sleep." +
+                                "Wake me up later when you're bored!");
+                        break;
+                    case 2:
+                        System.out.println("Heeeey " + friendName + " you're right!! But it's time for me to go to sleep." +
+                                "Wake me up later when you're bored!");
+                        break;
+                    case 3:
+                        System.out.println("Heeeey " + friendName + " you're right!!! But it's time for me to go to sleep." +
+                                "Wake me up later when you're bored!");
+                        break;
                 }
-                while (chooseMethod>0);
+                break;
+            }
+        }
+
+        for (; va == 1 || va == 2 || va == 3; ) {
+
+            switch (va) {
+                case 1:
+                    System.out.println("Heeeey " + friendName + " you're right! But it's time for me to go to sleep." +
+                            "Wake me up later when you're bored!");
+                    break;
+                case 2:
+                    System.out.println("Heeeey " + friendName + " you're right!! But it's time for me to go to sleep." +
+                            "Wake me up later when you're bored!");
+                    break;
+                case 3:
+                    System.out.println("Heeeey " + friendName + " you're right!!! But it's time for me to go to sleep." +
+                            "Wake me up later when you're bored!");
+                    break;
+            }
+            break;
         }
     }
+}
+
+
+
 
 
 
